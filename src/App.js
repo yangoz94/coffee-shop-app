3dfcdff5bd622df6fa420ce6c8223103ef26b50a
yangoz94 <95255319+yangoz94@users.noreply.github.com>
2022-10-23 01:28:18 -0400

    100vh bug fix on chrome and safari, mobile notification removal

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import OrderConfirmation from "./pages/OrderConfirmation-page/OrderConfirmation"
 import NotFound from "./pages/NotFound-page/NotFound";
 
 function App() {
+
   return (
     <>
       <ShoppingCartProvider>
--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -3,51 +3,61 @@ import "react-notifications-component/dist/theme.css";
 import "animate.css";
 
 function addItemToCartNotification(itemName) {
-  Store.addNotification({
-    title: "Added to cart",
-    message: `WOOHHOO, ${itemName} has been added to your cart!`,
-    type: "info", // Not a fan of success message color
-    insert: "top",
-    container: "top-right",
-    animationIn: ['animate__animated animate__bounceIn'], // `animate.css v4` classes
-    animationOut: ['animate__animated animate__zoomOut'],
-    dismiss: {
-      duration: 1500,
-      onScreen: true,
-    },
-  });
+  if (window.innerWidth > 1024) {
+    Store.addNotification({
+      title: "Added to cart",
+      message: `WOOHHOO, ${itemName} has been added to your cart!`,
+      type: "info", // Not a fan of success message color
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animate__animated animate__bounceIn"], // `animate.css v4` classes
+      animationOut: ["animate__animated animate__zoomOut"],
+      dismiss: {
+        duration: 1500,
+        onScreen: true,
+      },
+    });
+  }
 }
 
 function removeItemFromCartNotification(itemName) {
-  Store.addNotification({
-    title: "Removed from cart",
-    message: `NOOOOOOOOOO, ${itemName} has been removed from your cart`,
-    type: "danger",
-    insert: "top",
-    container: "top-left",
-    animationIn: ['animate__animated animate__bounceIn'], // `animate.css v4` classes
-    animationOut: ['animate__animated animate__zoomOut'],
-    dismiss: {
-      duration: 1500,
-      onScreen: true,
-    },
-  });
+  if (window.innerWidth > 1024) {
+    Store.addNotification({
+      title: "Removed from cart",
+      message: `NOOOOOOOOOO, ${itemName} has been removed from your cart`,
+      type: "danger",
+      insert: "top",
+      container: "top-left",
+      animationIn: ["animate__animated animate__bounceIn"], // `animate.css v4` classes
+      animationOut: ["animate__animated animate__zoomOut"],
+      dismiss: {
+        duration: 1500,
+        onScreen: true,
+      },
+    });
+  }
 }
 
 function clearCartNotification() {
-  Store.addNotification({
-    title: "Cart cleared",
-    message: "Your cart has been cleared",
-    type: "danger",
-    insert: "top",
-    container: "top-center",
-    animationIn: ['animate__animated animate__bounceIn'], // `animate.css v4` classes
-    animationOut: ['animate__animated animate__zoomOut'],
-    dismiss: {
-      duration: 2000,
-      onScreen: true,
-    },
-  });
+  if (window.innerWidth > 1024) {
+    Store.addNotification({
+      title: "Cart cleared",
+      message: "Your cart has been cleared",
+      type: "danger",
+      insert: "top",
+      container: "top-center",
+      animationIn: ["animate__animated animate__bounceIn"], // `animate.css v4` classes
+      animationOut: ["animate__animated animate__zoomOut"],
+      dismiss: {
+        duration: 2000,
+        onScreen: true,
+      },
+    });
+  }
 }
 
-export { addItemToCartNotification, removeItemFromCartNotification, clearCartNotification };
+export {
+  addItemToCartNotification,
+  removeItemFromCartNotification,
+  clearCartNotification,
+};
